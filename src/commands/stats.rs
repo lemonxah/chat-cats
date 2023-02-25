ef75a597394a3c900b176149bbd09ff0a61cb263
lemonxah <lemon.xah@gmail.com>
2023-02-25 11:28:00 +0200

    cargo.toml fixed

--- src/commands/stats.rs
+++ src/commands/stats.rs
@@ -20,7 +20,7 @@ impl StatsCommand {
             ], 
         }
     }
-    pub async fn respond(&self, message: &Message, _discord: &Discord, _db: Database) -> Result<Message, CommandError> {
+    pub async fn respond(&self, message: &Message, _cardiscord: &Discord, _db: Database) -> Result<Message, CommandError> {
         // let pc = discord.create_dm(message.author.id).unwrap();
         // discord.send_message(pc.id, &self.response.join("\n"), "", false).map_err(|e| e.into())
         Ok(message.clone())
