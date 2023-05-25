c37e8438eb1c965c8028aec8eea991a8a2c389a6
lemonxah <lemon.xah@gmail.com>
2023-05-25 13:28:39 +0200

    forgot the file :( my bad

---
+++ src/commands/definitions.rs
@@ -0,0 +1,50 @@
+use async_trait::async_trait;
+use discord::{model::Message, Discord};
+use macros::ChatCommand;
+use serde::Deserialize;
+use std::{result::Result, sync::Arc};
+use mongodb::Database;
+use super::{ChatCommand, CommandError, Responder, HelpCommands};
+
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct DefinitionsConfig {
+    response: Vec<String>,
+}
+
+#[derive(ChatCommand)]
+pub struct DefinitionsCommand {
+    matches: Vec<&'static str>,
+    config: DefinitionsConfig,
+}
+
+impl HelpCommands for DefinitionsCommand {
+    fn help() -> Vec<&'static str> {
+        vec![
+            "   Definitions - Responds with a Definitions message",
+            "       Example: cc definitions",
+        ]
+    }
+}
+
+#[async_trait]
+impl Responder for DefinitionsCommand {
+    type Config = DefinitionsConfig;
+    fn new(config: Self::Config) -> DefinitionsCommand {
+        DefinitionsCommand { 
+            matches: vec![
+                "Definitions",
+                "definitions",
+                "abv",
+                "explain",
+            ],
+            config,
+        }
+    }
+    async fn respond(&self, message: &Message, discord: Arc<Discord>, _db: Database) -> Result<Message, CommandError> {
+        let response = {
+            format!("{}", self.config.response.join("\n"))
+        };
+        discord.send_message(message.channel_id, &response, "", false).map_err(|e| e.into())
+    }
+}
